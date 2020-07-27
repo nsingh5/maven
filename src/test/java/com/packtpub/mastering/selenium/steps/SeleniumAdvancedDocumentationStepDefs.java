519ba2ec666e9c0b02920484cbb037a655eb817a
narensi5 <68420588+narensi5@users.noreply.github.com>
2020-07-27 09:25:10 +0530

    Update SeleniumAdvancedDocumentationStepDefs.java

--- src/test/java/com/packtpub/mastering/selenium/steps/SeleniumAdvancedDocumentationStepDefs.java
+++ src/test/java/com/packtpub/mastering/selenium/steps/SeleniumAdvancedDocumentationStepDefs.java
@@ -30,6 +30,7 @@ public class SeleniumAdvancedDocumentationStepDefs {
 
     @Before
     public void setUp() {
+        //test3
     	// System.setProperty("webdriver.gecko.driver","C:\\Users\\Narendra.Singh\\Downloads\\geckodriver.exe");
     //    driver = new FirefoxDriver();
 
